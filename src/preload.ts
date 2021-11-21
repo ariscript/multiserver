0851fbbe2b296b7026407533afacc5488c09bc27
Ari Prakash <ariprakash16@gmail.com>
2021-11-21 16:16:09 -0500

    get multi-window launch and IPC working

---
+++ src/preload.ts
@@ -0,0 +1,7 @@
+import { contextBridge, ipcRenderer } from "electron";
+
+contextBridge.exposeInMainWorld("instances", {
+    newInstanceWindow: () => {
+        ipcRenderer.send("newInstance");
+    },
+});
