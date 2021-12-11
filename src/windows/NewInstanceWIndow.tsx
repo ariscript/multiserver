6145bb18d6f7100c7aaf35f70b38e6a25782ad8a
Ari Prakash <ariprakash16@gmail.com>
2021-12-10 21:04:17 -0500

    set default version to 1.18.1

--- src/windows/NewInstanceWIndow.tsx
+++ src/windows/NewInstanceWIndow.tsx
@@ -12,7 +12,7 @@ import { Button, Checkbox, FormControlLabel, TextField } from "@mui/material";
 const NewInstanceWindow = () => {
     const [name, setName] = useState("");
     const [type, setType] = useState<InstanceOptions["type"]>("vanilla");
-    const [version, setVersion] = useState("1.18");
+    const [version, setVersion] = useState("1.18.1");
     const [javaPath, setJavaPath] = useState<string | undefined>();
     const [jvmArgs, setJvmArgs] = useState("");
 
