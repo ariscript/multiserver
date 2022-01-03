232ff2a212e5ecfebd20fd467cb4452144a957df
Dheeraj Prakash <dheerajpv16@gmail.com>
2022-01-02 19:26:16 -0500

    config: add DMG maker

--- forge.config.js
+++ forge.config.js
@@ -14,11 +14,13 @@ module.exports = {
         },
         {
             name: "@electron-forge/maker-zip",
-            platforms: ["darwin"],
+            platforms: ["darwin", "linux"],
         },
         {
             name: "@electron-forge/maker-deb",
-            config: {},
+        },
+        {
+            name: "@electron-forge/maker-dmg",
         },
     ],
     plugins: [
